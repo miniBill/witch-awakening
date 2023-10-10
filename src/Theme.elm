6b4841d87958e345aa72f69a01ef952fa808cbbb
Leonardo Taglialegne <cmt.miniBill@gmail.com>
2023-10-10 12:00:18 +0200

    Better layout for world shifts and game modes

--- src/Theme.elm
+++ src/Theme.elm
@@ -1,4 +1,4 @@
-module Theme exposing (backgroundColor, bebasNeue, blocks, borderColor, captureIt, card, celticHand, choice, classToColor, column, complicationCategoryToColor, complicationCategoryToGradient, gradientText, image, morpheus, padding, row, rythm, viewAffinity, wrappedRow)
+module Theme exposing (backgroundColor, bebasNeue, blocks, borderColor, captureIt, card, celticHand, choice, classToColor, colors, column, complicationCategoryToColor, complicationCategoryToGradient, gradientText, image, morpheus, padding, row, rythm, viewAffinity, wrappedRow)
 
 import Element exposing (Attribute, Element, centerY, el, fill, height, px, rgb, rgb255, text, width)
 import Element.Background as Background
--- src/View/Complications.elm
+++ src/View/Complications.elm
@@ -35,58 +35,77 @@ viewComplications complications =
 
             Complications make your life more difficult. {choice *Every Complication taken grants POWER shown in the corner.*}
             """
-        , [ brutality, masquerade, trueNames, monsters, population, bonk ]
-            |> List.map (complicationBox complications)
+        , [ complicationBox complications brutality
+          , complicationBox complications masquerade
+          , complicationBox complications trueNames
+          , complicationBox complications monsters
+          , complicationBox complications population
+          , complicationBox complications bonk
+          , Theme.blocks
+                [ width <| Element.maximum 400 fill
+                , alignTop
+                , Border.width 1
+                , Theme.padding
+                , Theme.borderColor Theme.colors.worldShift
+                ]
+                (String.Multiline.here """
+                    When taking world shifts, you're altering the nature of the particular version of Witch Awakening's reality that you enter into. The others may exist independently, but this one will be your home dimension.
+
+                    World shifts of course won't be seen in-universe as complications shown by Penelope, rather, they will be points of fact that Penelope points out similar to how she pointed out the information about the masquerade and other setting details.
+
+                    You can always choose if a World Shift affects the mundane and magical world alike, or only affects the magical world. (Only affecting the mundane world would be too inconsequential.)
+                    """)
+          ]
             |> Theme.wrappedRow
                 [ centerX
                 , spacing <| Theme.rythm * 3
                 ]
             |> Element.map (\( complication, selected ) -> ChoiceComplication complication selected)
-        , Theme.blocks [] <| String.Multiline.here """
-            When taking world shifts, you're altering the nature of the particular version of Witch Awakening's reality that you enter into. The others may exist independently, but this one will be your home dimension.
-
-            World shifts of course won't be seen in-universe as complications shown by Penelope, rather, they will be points of fact that Penelope points out similar to how she pointed out the information about the masquerade and other setting details.
-
-            You can always choose if a World Shift affects the mundane and magical world alike, or only affects the magical world. (Only affecting the mundane world would be too inconsequential.)
-            """
         , Theme.blocks [] <| String.Multiline.here """
             # Game mode
 
             {choice You can only choose one game mode, or none to play the default way. Each supports a different type of player, and what you might want out of it.}
             """
-        , [ storyArc
-          , earlyBird
-          , skillTree
-          , constellation
+        , [ complicationBox complications storyArc
+          , complicationBox complications earlyBird
+          , complicationBox complications skillTree
+          , complicationBox complications constellation
+          , Theme.blocks
+                [ width <| Element.maximum 400 fill
+                , alignTop
+                , Border.width 1
+                , Theme.padding
+                , Theme.borderColor Theme.colors.gameMode
+                ]
+                (String.Multiline.here """
+                    Slot modes ignore the requirement of 3 ➡️ 4 ➡️ 5 for acquiring magics. A slot is a slot and stands on its own, and is meant to be simplified
+                
+                    Slots mode is quite simplified.
+
+                    - (folk) {folk *FOLK*} slots can be used to obtain anything in the cyoa marked with the blue slot token.
+                    - (noble) {noble *NOBLE*} slots are a green token.
+                    - (heroic) {heroic *HEROIC*} slots use a gold token.
+                    - (epic) {epic *EPIC*} slots use a purple token.
+                    - (white) {white White} tokens are for variable options granting or requiring slots of a custom value based on the Power granted or required of the option in question, can use multiple slots per individual segments of a variable option, like each 4p class of Summer School.
+
+                    *Folk* = 1-4p. *Noble* = 5-8p. *Heroic* = 9-12p. *Epic* = 13-20.
+
+                    You can also use this to break down any slot gains from sources that may have given you power later on.
+
+                    _*Complications*_ are marked with a dot of the type that complication grants.
+
+                    In _Constellation_ mode, complication dots can be stacked or broken down like Skill Tree is capable of doing.
+
+                    In _Skill Tree mode_, you can reserve the slots from complications for later use.
+
+                    *Options with a cost of 0* or less that would normally be free as a result of your class, can be treated as a white token “Free” slot.
+                    """)
           ]
-            |> List.map (complicationBox complications)
             |> Theme.wrappedRow
                 [ centerX
                 , spacing <| Theme.rythm * 3
                 ]
             |> Element.map (\( complication, selected ) -> ChoiceComplication complication selected)
-        , Theme.blocks [] "Slot modes ignore the requirement of 3->4->5 for acquiring magics. A slot is a slot and stands on its own, and is meant to be simplified"
-        , Theme.blocks [] <| String.Multiline.here """
-            Slots mode is quite simplified.
-
-            - (folk) {folk *FOLK*} slots can be used to obtain anything in the cyoa marked with the blue slot token.
-            - (noble) {noble *NOBLE*} slots are a green token.
-            - (heroic) {heroic *HEROIC*} slots use a gold token.
-            - (epic) {epic *EPIC*} slots use a purple token.
-            - (white) {white White} tokens are for variable options granting or requiring slots of a custom value based on the Power granted or required of the option in question, can use multiple slots per individual segments of a variable option, like each 4p class of Summer School.
-
-            *Folk* = 1-4p. *Noble* = 5-8p. *Heroic* = 9-12p. *Epic* = 13-20.
-
-            You can also use this to break down any slot gains from sources that may have given you power later on.
-
-            _*Complications*_ are marked with a dot of the type that complication grants.
-
-            In _Constellation_ mode, complication dots can be stacked or broken down like Skill Tree is capable of doing.
-
-            In _Skill Tree mode_, you can reserve the slots from complications for later use.
-
-            *Options with a cost of 0* or less that would normally be free as a result of your class, can be treated as a white token “Free” slot.
-        """
         ]
 
 
