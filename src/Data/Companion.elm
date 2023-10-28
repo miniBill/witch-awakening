cb724223f4d102aa4a8d6c81be0e99daecd63dfb
Leonardo Taglialegne <cmt.miniBill@gmail.com>
2023-10-28 20:45:51 +0200

    One more note on companions

--- src/Data/Companion.elm
+++ src/Data/Companion.elm
@@ -39,7 +39,7 @@ intro =
     """
     It’s dangerous to go alone, take this! A Companions section. {choice You have one free companion from your chosen faction, and one free that shares the same witch type} as you (either Race or Class). Companions have 4 main statistics giving a _rough idea_ of their general impact in a group dynamic, except the {choice POWER stat, which primarily serves to give free floating Power points to spend to _customize_ your companion’s abilities} on top of the default abilities they come with. Beyond that, interactions are inferred in how you interpret their personality and how different stats play out in relation to it and others. {choice You can buy additional companions using Reward Points}, listed to the left of their name. A {choice +} indicates they have their Type Perk.
 
-    You can spend your own Power on behalf of a member if you so choose.
+    {choice You can spend your own Power on behalf of a member if you so choose, although they receive 1/2 the Reward value of Quests, so you don't _need to_.}
 
     **Really, don’t stress the details too much and feel free to fill in the blanks. I do what I can to provide a reasonable summary of an array of characters, but it’s up to you to interpret it as you will.**
 
