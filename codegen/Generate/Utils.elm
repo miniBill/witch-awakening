c2b72e879e04d8170a1d090d1cd114c141ace54c
Leonardo Taglialegne <cmt.miniBill@gmail.com>
2025-07-07 17:48:31 +0200

    Add documentation

--- codegen/Generate/Utils.elm
+++ codegen/Generate/Utils.elm
@@ -3,6 +3,8 @@ module Generate.Utils exposing (yassify)
 import String.Extra
 
 
+{-| Call `String.Extra.classify`, after replacing æ => ae, Æ => AE and stripping `"`.
+-}
 yassify : String -> String
 yassify str =
     str
