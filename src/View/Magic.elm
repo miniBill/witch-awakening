63ffd3cb883ab65eca2dc7cb49c7059dafa86e3d
Leonardo Taglialegne <cmt.miniBill@gmail.com>
2025-09-21 10:52:59 +0200

    Remove unused imports

--- src/View/Magic.elm
+++ src/View/Magic.elm
@@ -1,7 +1,7 @@
 module View.Magic exposing (magicBox, viewMagics)
 
 import Data.Magic as Magic exposing (Affinities(..))
-import Element exposing (Element, centerX, centerY, column, el, fill, fillPortion, height, moveDown, moveRight, moveUp, px, rgb, rgba, shrink, width)
+import Element exposing (Element, centerX, centerY, column, el, fill, fillPortion, height, moveDown, moveUp, px, rgb, rgba, width)
 import Element.Background as Background
 import Element.Border as Border
 import Element.Font as Font
