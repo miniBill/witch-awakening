d883e0b914fd5b5f267ba45892c1e4c02b013d4e
Leonardo Taglialegne <cmt.miniBill@gmail.com>
2025-09-25 17:32:07 +0200

    Remove unused file

--- src/View/GameMode.elm
+++ src/View/GameMode.elm
@@ -1,7 +1,6 @@
 module View.GameMode exposing (viewGameMode)
 
 import Color exposing (Color)
-import Data.GameMode as GameMode
 import Element exposing (Element, alignBottom, alignTop, centerX, el, fill, moveDown, moveUp, spacing, width)
 import Element.Border as Border
 import Element.Font as Font
@@ -36,7 +35,7 @@ viewGameMode hideDLC display gameMode =
                     , Theme.padding
                     , Theme.borderColor Theme.colors.gameMode
                     ]
-                    GameMode.slotDescription
+                    slotDescription
 
             boxes : List (Element (Maybe GameMode))
             boxes =
@@ -48,7 +47,7 @@ viewGameMode hideDLC display gameMode =
             DisplayGameMode
             ChoiceGameMode
             "# Game Mode"
-            [ Theme.blocks [] GameMode.intro
+            [ Theme.blocks [] intro
             , (boxes ++ [ slotsBox ])
                 |> Theme.wrappedRow
                     [ centerX
@@ -131,3 +130,37 @@ gameModeBox display selected { name, content } =
         , content = [ Theme.blocks [] content ]
         , onPress = Just msg
         }
+
+
+intro : String
+intro =
+    """
+    {choice You can only choose one game mode, or none to play the default way. Each supports a different type of player, and what you might want out of it.}
+    """
+
+
+slotDescription : String
+slotDescription =
+    """
+    Slot modes ignore the requirement of 3 ➡️ 4 ➡️ 5 for acquiring magics. A slot is a slot and stands on its own, and is meant to be simplified
+
+    Slots mode is quite simplified.
+
+    - (folk) {folk *FOLK*} slots can be used to obtain anything in the cyoa marked with the blue slot token.
+    - (noble) {noble *NOBLE*} slots are a green token.
+    - (heroic) {heroic *HEROIC*} slots use a gold token.
+    - (epic) {epic *EPIC*} slots use a purple token.
+    - (white) {white White} tokens are for variable options granting or requiring slots of a custom value based on the Power granted or required of the option in question, can use multiple slots per individual segments of a variable option, like each 4p class of Summer School.
+
+    *Folk* = 1-4p. *Noble* = 5-8p. *Heroic* = 9-12p. *Epic* = 13+.
+
+    You can also use this to break down any slot gains from sources that may have given you power later on.
+
+    _*Complications*_ are marked with a dot of the type that complication grants.
+
+    In _Constellation_ mode, complication dots can be stacked or broken down like Skill Tree is capable of doing.
+
+    In _Skill Tree mode_, you can reserve the slots from complications for later use.
+
+    *Options with a cost of 0* or less that would normally be free as a result of your class, can be treated as a white token “Free” slot.
+    """
