703bb85c6b447491ed37160f99db9033d407a0cf
Leonardo Taglialegne <cmt.miniBill@gmail.com>
2025-10-18 16:45:51 +0200

    Genies need to take their type perk

--- src/Data/Costs/TypePerks.elm
+++ src/Data/Costs/TypePerks.elm
@@ -2,6 +2,7 @@ module Data.Costs.TypePerks exposing (value)
 
 import Data.Costs.Monad as Monad exposing (Monad)
 import Data.Costs.Utils as Utils exposing (Points)
+import Data.Race as Race
 import Generated.TypePerk
 import Generated.Types as Types exposing (Race)
 import Types exposing (IdKind(..), Model)
@@ -13,6 +14,17 @@ value model =
     model.typePerks
         |> Monad.mapAndSum (typePerkCost model)
         |> Monad.map (\p -> Utils.powerToPoints -p)
+        |> checkForGenie model
+
+
+checkForGenie : Model key -> Monad p -> Monad p
+checkForGenie model p =
+    if List.any Race.isGenie model.races && not (List.any Race.isGenie model.typePerks) then
+        p
+            |> Monad.withWarning "Genies need to take their type perk"
+
+    else
+        p
 
 
 typePerkCost : Model key -> Race -> Monad Int
