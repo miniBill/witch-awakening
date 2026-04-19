1c2285c43d2d37d3b451ab6514ea727f273d74c1
Leonardo Taglialegne <leonardo@taglialegne.it>
2026-04-19 17:34:08 +0200

    Add class name to menu

--- src/Data/Costs/Class.elm
+++ src/Data/Costs/Class.elm
@@ -2,12 +2,24 @@ module Data.Costs.Class exposing (value)
 
 import Data.Costs.Monad as Monad exposing (Monad)
 import Data.Costs.Points as Points exposing (Points)
+import Generated.Class as Class
 import Generated.Types exposing (Class(..))
-import Types exposing (Model)
+import Types exposing (IdKind(..), Model)
 
 
 value : Model key -> Monad Points
 value model =
+    case model.class of
+        Nothing ->
+            innerValue model
+
+        Just class ->
+            innerValue model
+                |> Monad.withPointsInfo IdKindClass (Class.toString class)
+
+
+innerValue : Model key -> Monad Points
+innerValue model =
     case model.class of
         Just class ->
             case class of
